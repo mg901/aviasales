924f2b0bced3406206cbb4e248e0ffdc64e6d788
mg901 <mgreen097@gmail.com>
2019-12-23 03:53:54 +0300

    refactor: fix border-radius, set className and callback

--- src/ui/tab/index.tsx
+++ src/ui/tab/index.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React from 'react';
+import './index.css';
 
 type Props = {
   children: React.ReactNode;
+  onClick: () => void;
 };
 
-export const Tab: React.FC<Props> = ({ children }) => (
-  <button type="button">{children}</button>
+export const Tab: React.FC<Props> = ({ onClick, children }) => (
+  <button type="button" className="tab" onClick={onClick}>
+    {children}
+  </button>
 );
