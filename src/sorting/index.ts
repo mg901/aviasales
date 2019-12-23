ac3386cbf45570086e7566a991e02484b8e7dd4f
mg901 <mgreen097@gmail.com>
2019-12-23 04:39:49 +0300

    chore: add sorting

---
+++ src/sorting/index.ts
@@ -0,0 +1,1 @@
+export { Sorting } from './sorting';
---
+++ src/sorting/sorting.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Tabs, Tab } from '../ui';
+
+export const Sorting = () => (
+  <Tabs>
+    <Tab onClick={() => {}}>Самый дешёвый</Tab>
+    <Tab onClick={() => {}}>Самый Быстрый</Tab>
+  </Tabs>
+);
