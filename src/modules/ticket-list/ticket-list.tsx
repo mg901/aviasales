0321b5b272a1576f1e2a7e32b99f04b9ba82148b
mg901 <mgreen097@gmail.com>
2020-01-04 11:56:16 +0300

    refactor: replace filteredTickets with sortedTickets

--- src/modules/ticket-list/ticket-list.tsx
+++ src/modules/ticket-list/ticket-list.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import { useStore, useList } from 'effector-react';
+import { useList } from 'effector-react';
 import { $visibleTickets } from './model';
 import { Ticket } from '../../ui';
-import { $isEmptyCache } from '../cache';
 
-export const TicketsList: React.FC = () => {
-  const isEmptyCache = useStore($isEmptyCache);
-
-  const tickets = useList($visibleTickets, ({ price, carrier }) => (
-    <li>
-      <Ticket price={price.title} carrier={carrier} />
-    </li>
-  ));
-
-  return isEmptyCache ? <ul>{tickets}</ul> : null;
-};
+export const TicketsList: React.FC = () => (
+  <ul>
+    {useList($visibleTickets, ({ priceTitle, carrier }) => (
+      <li>
+        <Ticket price={priceTitle} carrier={carrier} />
+      </li>
+    ))}
+  </ul>
+);
